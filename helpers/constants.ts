cca2baa0fda77b125b638f2822f93084f7b64353
dsactum <ds@actumdigital.com>
2023-05-23 17:14:24 +0200

    change type

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -1,13 +1,13 @@
-export const WSAG_VALUES = {
-  "line-height": "1.5",
-  "letter-spacing": "0.12",
-  "word-spacing": "0.16",
-  "paragraph-spacing": "2"
-}
-
 export type TStyle = {
   "line-height": string
   "letter-spacing": string
   "word-spacing": string
   "paragraph-spacing": string
+}
+
+export const WSAG_VALUES: TStyle = {
+  "line-height": "1.5",
+  "letter-spacing": "0.12",
+  "word-spacing": "0.16",
+  "paragraph-spacing": "2"
 }
