800e997768f2e500e2d958f49ef12f939d1e99d9
dsactum <ds@actumdigital.com>
2023-05-23 19:36:55 +0200

    refactor Controls

--- popup/Controls/index.tsx
+++ popup/Controls/index.tsx
@@ -1,19 +1,43 @@
 import { useState } from "react"
 
+import { useStorage } from "@plasmohq/storage/hook"
+
+import { DEFAULT_VALUES, type TStyle } from "~helpers/constants"
+
 import { Button } from "./Button"
 import { Slider } from "./Slider"
 
 export const Controls = () => {
   const [message, setMessage] = useState("")
+  // Set default storage values
+  const [styles, setStyles, { setRenderValue }] = useStorage<TStyle>(
+    "styles",
+    (v) => (v === undefined || null ? DEFAULT_VALUES : v)
+  )
+  const [enabled, setEnabled] = useStorage<boolean>("enabled", (v) =>
+    v === undefined || null ? true : v
+  )
   return (
     <>
-      <Button setMessage={setMessage} />
+      <Button
+        styles={styles}
+        enabled={enabled}
+        setStyles={setStyles}
+        setEnabled={setEnabled}
+        setRenderValue={setRenderValue}
+        setMessage={setMessage}
+      />
 
       <p role="alert" id="message">
         {message}
       </p>
 
-      <Slider />
+      <Slider
+        styles={styles}
+        enabled={enabled}
+        setStyles={setStyles}
+        setRenderValue={setRenderValue}
+      />
     </>
   )
 }
